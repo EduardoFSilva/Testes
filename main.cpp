2bfed1951ef4dca974b1cacb1c2b0959124292dc
EduardoFSilva <eduardofernandes2107@gmail.com>
2019-09-26 17:46:21 -0300

    Adicionado Printf no JNI

--- main.cpp
+++ main.cpp
@@ -4,17 +4,21 @@
 #include <string.h>
 
 JNIEXPORT jfloat JNICALL Java_NativeCalc_add(JNIEnv* env, jobject obj, jfloat val1, jfloat val2) {
+	printf("Sum From Native");
     return val1 + val2;
 }
 
 JNIEXPORT jfloat JNICALL Java_NativeCalc_subtract(JNIEnv* env, jobject obj, jfloat val1, jfloat val2) {
+	printf("Subtract From Native");
     return val1 - val2;
 }
 
 JNIEXPORT jfloat JNICALL Java_NativeCalc_multiply(JNIEnv* env, jobject obj, jfloat val1, jfloat val2) {
+	printf("Multiply From Native");
     return val1 * val2;
 }
 
-JNIEXPORT jfloat JNICALL Java_NativeCalc_divide(JNIEnv* env, jobject obj, jfloat val1, jfloat val2) {
+JNIEXPORT jfloat JNICALL Java_NativeCalc_divide(JNIEnv* env, jobject obj, jfloat val1, jfloat val2) 
+	printf("Division From Native");
     return val1 / val2;
 }
